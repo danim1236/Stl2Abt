d6463f57e1965a1ac4ad05aa692863d6b7dcc97a
Daniel Mendes <daniel@danim.com.br>
2013-12-27 21:15:00 -0200

    Criando o projeto para lidar com Stl e seu projeto de testes, inclusive nunit e shaptestex

---
+++ BioGenie.Stl.Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioGenie.Stl.Tests
+{
+    public class Class1
+    {
+    }
+}
