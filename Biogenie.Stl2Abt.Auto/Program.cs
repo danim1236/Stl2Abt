3450cd5382e130a416242a414a243c58ac6f6f24
Daniel Mendes <daniel@danim.com.br>
2016-09-19 16:21:48 -0300

    Projeto para console

---
+++ Biogenie.Stl2Abt.Auto/Program.cs
@@ -0,0 +1,29 @@
+﻿using BioGenie.Stl;
+using System;
+using System.IO;
+using BioGenie.Stl.Algorithm;
+
+namespace Biogenie.Stl2Abt.Auto
+{
+    class Program
+    {
+        private static Stl2AbtManager _stl2AbtManager;
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Use Biogenie.Stl2Abt.Auto [arquivo.abt]");
+            }
+            args = new[] {args[0], Path.GetFileNameWithoutExtension(args[0]) + ".abt"};
+
+            string stlFileName = args[0];
+            string abtFileName = Path.GetFileNameWithoutExtension(args[0]) + ".abt";
+
+            _stl2AbtManager = new Stl2AbtManager(stlFileName, abtFileName);
+            _stl2AbtManager.GenerateModel(9);
+
+            Abt.WriteAbt(_stl2AbtManager.AbtFileName, _stl2AbtManager.AbtBoundary);
+        }
+    }
+}
